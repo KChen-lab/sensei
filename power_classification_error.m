ef9d923c25e71362ea4ec1f6328a932a1e856c8c
Liang <SLiang3@mdanderson.org>
2020-03-11 13:33:15 -0500

    update

--- power_classification_error.m
+++ power_classification_error.m
@@ -1,4 +1,4 @@
-M = [20, 20];
+M = [10, 10];
 N = [1000, 1000];
 
 a = [7, 10];
@@ -6,6 +6,8 @@ b = [10, 7];
 
 alpha = 0.05;
 
+q_FN = 0.2;
+q_FP = 0.1;
 %figure;
 %hold on;
 %x = 0:.01:1;
@@ -13,8 +15,8 @@ alpha = 0.05;
 %    plot(x, betapdf(x, a(i), b(i)))
 %end
 
-Ep = a ./ (a + b);
-Vp = a .* b .* (a + b + N) ./ ((N .* (a + b) .^ 2) .* (a + b + 1));
+Ep = (1 - q_FP - q_FN) * a ./ (a + b) + q_FP;
+Vp = (1 - q_FP - q_FN) ^ 2 * a .* b .* (a + b + N) ./ ((N .* (a + b) .^ 2) .* (a + b + 1));
 
 S = sqrt(sum((M - 1) .* Vp) ./ (sum(M) - 2));
 
